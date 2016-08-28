7aa74b5c77b04602089217989b86198d33a35a57
dragoped <danylowd@gmail.com>
2016-08-28 20:25:36 +0300

    Add files via upload

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+def echo(text)
+	text
+end
+def shout(text)
+	text.upcase
+end
+def repeat(text, times = 2)
+	([text] * times).join(" ")
+end
+def start_of_word(text, amount)
+	text[0...amount]
+end
+def first_word(text)
+	text.split(' ').first
+end
+def titleize(text)
+ words = text.split.map { |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  }
+  words.first.capitalize!
+  words.join(" ")
+end
