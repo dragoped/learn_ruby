9e9b5b059b937cfb6e6d233628c799704cd05349
dragoped <danylowd@gmail.com>
2016-08-30 14:50:50 +0300

    Add files via upload

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_accessor :title
+  def title=(ntitle)
+   words = ntitle.split(" ")
+   words = [words[0].capitalize] + words[1..-1].map {|x| 
+   	little_word = %w{a an the of for from in and}
+   if little_word.include? x
+   	x
+   else
+   	x.capitalize
+   end
+   }
+   @title = words.join(' ')
+  end
 end
