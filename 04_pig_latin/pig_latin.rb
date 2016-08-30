4628295c4c4192d59757686f6a3213a973cbd27f
dragoped <danylowd@gmail.com>
2016-08-30 14:00:46 +0300

    Add files via upload

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,19 @@
 #write your code here
+def translate(s)
+  $vowels = ['a','e','i','o','u','y']
+  s.split.map do |word|
+    i = 0
+    while not vowel? word[i,1]
+      if ( word[i,2] == 'qu')
+        i += 2
+      else
+        i += 1
+      end
+    end
+    word[i..-1] + word[0,i] + 'ay'
+  end.join(' ')
+end
+
+def vowel? c
+  $vowels.include? c
+end
